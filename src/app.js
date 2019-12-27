4e7a40c042511bbbca204fdff5225a8c7d8ec9e5
unknown <a.khoshbakht79@gmail.com>
2019-12-27 19:05:41 +0300

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,11 +12,6 @@ import './styles/style.scss';
 
 const store = storeConfig();
 
-store.dispatch(addExpense({ description: 'rent', amount: 21000, createdAt: 1200 }));
-store.dispatch(addExpense({ description: 'coffeeeee', amount: 100, createdAt: 123412 }));
-store.dispatch(addExpense({ description: 'tea', createdAt: 1000000 }));
-store.dispatch(addExpense({ description: 'xxx' }));
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
